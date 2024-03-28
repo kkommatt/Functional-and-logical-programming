6d7a8a417ce8001795207c62093def76d86c89e1
Matthew <100029063+kkommatt@users.noreply.github.com>
2024-03-28 12:47:54 +0200

    fix: output one list with sublists

--- lab2_pl.pl
+++ lab2_pl.pl
@@ -1,23 +1,19 @@
 split_list(InputList, OutputLists) :-
-    reverse(InputList, ReversedInputList), 
-    split_list_helper(ReversedInputList, 1, OutputLists).
+    reverse(InputList, ReversedInputList),
+    split_list_helper(ReversedInputList, 1, [], OutputLists).
 
-split_list_helper([], _, []).
-split_list_helper(InputList, N, OutputLists) :-
+split_list_helper([], _, Accumulator, OutputLists) :-
+    reverse(Accumulator, OutputLists).
+split_list_helper(InputList, N, Accumulator, OutputLists) :-
     take_power_elements(InputList, N, SubList, Remaining),
-    reverse(SubList, ReversedSubList), 
-    length(ReversedSubList, SubListLength),
+    reverse(SubList, ReversedSubList),
+    append(Accumulator, [ReversedSubList], NewAccumulator),
     NextN is N + 1,
-    split_list_helper(Remaining, NextN, RestOutputLists),
-    append(RestOutputLists, [ReversedSubList], OutputLists), 
-    format('Sub-list with ~d elements: ~w~n', [SubListLength, ReversedSubList]).
-
-
-
+    split_list_helper(Remaining, NextN, NewAccumulator, OutputLists).
 
 take_power_elements(List, N, Taken, Remaining) :-
     length(List, ListLength),
-    Nth is N ** N,  
+    Nth is N ** N,
     (ListLength >= Nth ->
         take(Nth, List, Taken, Remaining)
     ;
@@ -30,14 +26,14 @@ take(N, [X|Xs], [X|Ys], Zs) :-
     N1 is N - 1,
     take(N1, Xs, Ys, Zs).
 
-
 main :-
     write('Enter a list of integers separated by space: '),
     read_line_to_codes(user_input, Input),
     atom_codes(Atom, Input),
     atomic_list_concat(StrList, ' ', Atom),
     maplist(atom_number, StrList, InputList),
-    split_list(InputList, _),
+    split_list(InputList, OutputLists),
+    format('Output list: ~w~n', [OutputLists]),
     halt.
 
 :- initialization(main).
